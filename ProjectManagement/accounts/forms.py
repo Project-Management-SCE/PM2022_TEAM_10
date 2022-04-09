18bfadc3726bb663d8bb531add2f5529a02659c8
eizikr <eizikrrr@gmail.com>
2022-04-09 17:09:46 +0300

    Account association manager signup BSPM2022T10-93

---
+++ ProjectManagement/accounts/forms.py
@@ -0,0 +1,35 @@
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .models import User , associationManager
+from django.db import transaction
+
+class AssociationManagerSignUpform(UserCreationForm):
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)
+    id_number = forms.CharField(required=True)
+    phone_number = forms.CharField(required=True)
+    association_number = forms.CharField(required=True)
+    email = forms.EmailField(max_length=100)
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'id_number', 'phone_number', 'association_number')
+
+    @transaction.atomic
+    def save(self):
+        user = super().save(commit=False)
+        user.is_association_manager = True
+        user.is_active = False
+        user.username = self.cleaned_data.get('username')
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.email = self.cleaned_data.get('email')
+        user.phone_number = self.cleaned_data.get('phone_number')
+        user.id_number = self.cleaned_data.get('id_number')
+        user.save()
+
+        assoManager = associationManager.objects.create(user=user)
+        assoManager.association_number = self.cleaned_data.get('association_number')
+
+        assoManager.save()
+        return user
