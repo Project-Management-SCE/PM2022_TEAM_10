375d9fddbe127c8e4678e25e095b6687cf7fb5e4
daniel1612 <danieldahan220715@gmail.com>
2022-05-02 13:37:22 +0300

    HelpoUser can watch posts and edit v1.0

--- ProjectManagement/posts/views.py
+++ ProjectManagement/posts/views.py
@@ -1,7 +1,9 @@
-from posts.models import Category
+from posts.models import Category,Post
+from accounts.models import HelpoUser
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
-from .forms import createPostForm
+from .forms import createPostForm, editPostForm
+
 import datetime
 # Create your views here.
 
@@ -26,3 +28,42 @@ def createPost(request):
 
     context={'form':form,'user_obj':user_obj}
     return render(request, 'createPostForm.html', context)
+
+def showMyPosts(request, pk):
+    user = HelpoUser.objects.get(user_id = pk)
+
+    if request.user != user.user:
+        return render(request,"error_page.html",{})
+
+    posts = Post.objects.all().filter(user = user).order_by('date')
+
+    context = {
+        'posts':posts,
+        'user_obj':request.user
+    }
+    
+    return render(request, 'myPosts.html', context)
+
+def editPost(request, pk):
+    post = Post.objects.get(id=pk)  
+
+    user_obj = HelpoUser.objects.get(user = post.user)
+    
+    if request.user != user_obj.user:
+        return render(request,"error_page.html",{})
+
+    if request.method == 'POST':
+        form = editPostForm(request.POST, instance=post)
+
+        if form.is_valid():
+            form.save()
+            return redirect('showMyPosts',pk = user_obj.user_id)
+
+    else:
+        form = editPostForm(instance=post)
+    
+    context = {
+                'form' : form,
+                'obj':user_obj,
+            }
+    return render(request,"editPost.html",context)
