f238f1b47992739d0c1d816d7acff73da149f7e0
daniel1612 <danieldahan220715@gmail.com>
2022-05-22 00:13:50 +0300

    Finished UT for US admin can edit Questions and Answers BSPM2022T10-360 BSPM2022T10-347

--- ProjectManagement/adminPanel/tests/test_urls.py
+++ ProjectManagement/adminPanel/tests/test_urls.py
@@ -1,13 +1,12 @@
 from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from adminPanel.views import searchAsso,AllFeedbacks
-from adminPanel.views import searchAsso,showActivityTracking
+from adminPanel.views import searchAsso,showActivityTracking,show_questions,add_question,delete_question, edit_question
 
 class TestUrls(SimpleTestCase):
     def test_searchAsso_url_is_resolved(self):
         url = reverse('searchAsso')
-        self.assertEqual(resolve(url).func, searchAsso)
-        
+        self.assertEqual(resolve(url).func, searchAsso)   
         
     def test_AllFeedbacks_url_is_resolved(self):
         url = reverse('AllFeedbacks')
@@ -17,4 +16,18 @@ class TestUrls(SimpleTestCase):
         url = reverse('showActivityTracking')
         self.assertEqual(resolve(url).func, showActivityTracking)   
 
+    def test_show_questions_url_is_resolved(self):
+        url = reverse('show_questions')
+        self.assertEqual(resolve(url).func, show_questions)   
         
+    def test_add_question_url_is_resolved(self):
+        url = reverse('add_question')
+        self.assertEqual(resolve(url).func, add_question)   
+
+    def test_delete_question_url_is_resolved(self):
+        url = reverse('delete_question',kwargs={'pk':'1'})
+        self.assertEqual(resolve(url).func, delete_question)   
+
+    def test_edit_question_url_is_resolved(self):
+        url = reverse('edit_question',kwargs={'pk':'1'})
+        self.assertEqual(resolve(url).func, edit_question)   
