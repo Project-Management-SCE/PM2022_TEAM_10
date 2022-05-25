d5dccbfe05b548cd3689cfd4446386aa454aff93
gilbenhamo <gilbh859@gmail.com>
2022-05-25 21:52:29 +0300

    continue Asso photos, now can edit and delete BSPM2022T10-331 BSPM2022T10-333 BSPM2022T10-336 BSPM2022T10-337 BSPM2022T10-338 BSPM2022T10-339

--- ProjectManagement/home/forms.py
+++ ProjectManagement/home/forms.py
@@ -2,16 +2,17 @@ from django import forms
 from home.models import Image
 
 class ImageFrom(forms.ModelForm):
-    # img = forms.ImageField(
-    #     label="Image",
-    #     widget=forms.ClearableFileInput(
-    #         attrs={"multiple":True},
-    #     )
-    # )
     class Meta():
         model = Image
         fields = ['img',]
+        widgets = {
+            'img':forms.ClearableFileInput(
+                attrs={
+                    'dir': 'rtl',
+                },
+            )
+        }
         labels = {
-            "img":'תמונה',
+            "img":'',
         }
         
